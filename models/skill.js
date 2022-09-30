9deda991ee6b10f505ecb27574780c670a028cc3
mGrimley <mich@elgrimley.com>
2022-09-30 14:11:39 -0700

    Updated create(skill)

--- models/skill.js
+++ models/skill.js
@@ -19,16 +19,13 @@ function getAll() {
 }
 
 function getOne(id) {
-    // URL params are strings - convert to a number
     id = parseInt(id);
-    // The Array.prototype.find iterator method is
-    // ideal for finding objects within an array
     return skills.find(skill => skill.id === id);
 }
 
 function create(skill) {
     skill.id = Date.now() % 1000000
-    skill.done = false
+    skill.yearsExp = 0
     skills.push(skill)
 }
 
